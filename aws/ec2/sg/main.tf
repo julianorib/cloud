55490013d9d3bf99dcdac4fcf2288eb4792999c9
Juliano Ribeiro Silva <julianorib@gmail.com>
2025-03-18 05:38:39 -0300

    AWS EC2 Security Group

--- aws/ec2/sg/main.tf
+++ aws/ec2/sg/main.tf
@@ -11,3 +11,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
+## Criar um Security Group de acesso Internet
+resource "aws_security_group" "acesso-out-internet" {
+  name        = "acesso-out-internet"
+  description = "permite acesso out internet"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    description = "all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## Criar um Security Group de acesso SSH
+resource "aws_security_group" "acesso-in-ssh" {
+  name        = "acesso-in-ssh"
+  description = "permite acesso in ssh"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
